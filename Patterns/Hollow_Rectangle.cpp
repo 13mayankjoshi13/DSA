36d15fa5425489ed5487c08159e6bd27df298fc7
Mayank Joshi <125684136+13mayankjoshi13@users.noreply.github.com>
2026-09-03 22:33:45 +0530

    Add hollow rectangle pattern printing in C++

---
+++ Patterns/Hollow_Rectangle.cpp
@@ -0,0 +1,10 @@
+int rows = 4, cols = 6;
+for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= cols; j++) {
+        if (i == 1 || i == rows || j == 1 || j == cols)
+            cout << "*";
+        else
+            cout << " ";
+    }
+    cout << endl;
+}
