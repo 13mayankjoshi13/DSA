63abc29a3561913f9cf55a1599e469b63421ae53
Mayank Joshi <125684136+13mayankjoshi13@users.noreply.github.com>
2026-09-08 22:36:56 +0530

    Modify stringANDget.cpp for file input/output
    Updated stringANDget.cpp to read input from a file and output to another file.

--- stringANDget.cpp
+++ stringANDget.cpp
@@ -1,4 +1,8 @@
-String S1;
-String S2;
+Input.txt file has  Hey Striver
+
+String S1; //s1 takes hey only 
+String S2; // s2 will take striver 
 cin >> s1 >> s2;
 cout << s1 << " " << s2;
+
+Output.txt  file will show Hey Striver
