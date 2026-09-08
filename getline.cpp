04573f017e4724f9e52512a47818513d21ad1725
Mayank Joshi <125684136+13mayankjoshi13@users.noreply.github.com>
2026-09-08 22:40:39 +0530

    Implement getline to read full line input
    Added code to read a whole line from input using getline.

---
+++ getline.cpp
@@ -0,0 +1,7 @@
+String Str; //{now str will read whole line}
+getline(cin,str);
+cout << str;
+
+//but if Input.txt has 
+Hey Striver //getline will not read both lines in one variable
+I am Mayank 
