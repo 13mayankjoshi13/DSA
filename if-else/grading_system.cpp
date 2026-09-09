216b5e89445dab56e3616812f80ef093ded4dbb2
Mayank Joshi <125684136+13mayankjoshi13@users.noreply.github.com>
2026-09-09 14:45:09 +0530

    Add grading system based on marks
    Implement a grading system based on user input marks.

---
+++ if-else/grading_system.cpp
@@ -0,0 +1,28 @@
+//a school has following rules for grading system
+/*Below 25 - F 
+25 to 44 - E
+45 to 49 -D
+50 to 59 -C
+60 to 79 -B
+80 to 100 -A
+Ask user to enter marks and print grade
+*/
+int main(){
+  int marks;
+  cin >> marks;
+  if(marks<25){
+    cout << "F';
+  }
+  if (marks >=25 && marks <=44){
+    cout << "E";
+  }
+  :
+  :
+  :
+  : 
+  :
+  if (marks >=80 && marks <=100){
+    cout << "A";
+  }
+  return 0;
+}
