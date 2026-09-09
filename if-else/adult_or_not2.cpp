346d01536cf9d085d4f14371a5aba62d51a5d62c
Mayank Joshi <125684136+13mayankjoshi13@users.noreply.github.com>
2026-09-09 14:52:40 +0530

    Add comments to adult_or_not2.cpp for optimization
    Add comments explaining the need for using else if to optimize code execution.

---
+++ if-else/adult_or_not2.cpp
@@ -0,0 +1,3 @@
+// As we did in [adult_or_not.cpp](./adult_or_not.cpp)
+// code is correct but not the write way to write , as it will execute all the if statements
+// it will take a lot of space and time , hence , we will use else if to check only certain statements
