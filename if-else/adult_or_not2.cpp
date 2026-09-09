83658e986b8548bbefd2a2b18baeb571d6af0f2b
Mayank Joshi <125684136+13mayankjoshi13@users.noreply.github.com>
2026-09-09 14:55:55 +0530

    Update grading system logic i
    Refactor grading logic to use else if statements for efficiency.

--- if-else/adult_or_not2.cpp
+++ if-else/adult_or_not2.cpp
@@ -1,3 +1,26 @@
-// As we did in [adult_or_not.cpp](./adult_or_not.cpp)
+ //As we did in [grading_system.cpp](./grading_system.cpp)
 // code is correct but not the write way to write , as it will execute all the if statements
 // it will take a lot of space and time , hence , we will use else if to check only certain statements
+int main() {
+    int marks;
+    
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    if (marks < 25)
+        cout << "Grade: F";
+    else if (marks <= 44)
+        cout << "Grade: E";
+    else if (marks <= 49)
+        cout << "Grade: D";
+    else if (marks <= 59)
+        cout << "Grade: C";
+    else if (marks <= 79)
+        cout << "Grade: B";
+    else if (marks <= 100)
+        cout << "Grade: A";
+    else
+        cout << "Invalid marks";
+
+    return 0;
+}
